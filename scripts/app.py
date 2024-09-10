77cffe67b9e52230262e866189457796d2329e71
kaleab <kaleabzegeye1@gmail.com>
2024-09-10 14:48:06 +0300

    the preprosessing for the data is implemented also the data is imported from google drive in csv format in order to be implemeted online on streamlit

--- scripts/app.py
+++ scripts/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from preprocessing import replace_missing_with_mean, replace_outliers_with_mean
 from customer_satisfaction_analysis import compute_satisfaction_score
 from reserv import dispersion_parameters, display_user_engagement_metrics, visualize_app_traffic, visualize_basic_metrics, visualize_correlation, visualize_decile_data, visualize_decliced_data, visualize_elbow_method, visualize_handset_stats, visualize_kmeans_clusters, visualize_pca, visualize_regression_model, visualize_silhouette_score, visualize_top_3_apps
 import plotly.express as px 
@@ -10,8 +11,41 @@ from load_data import load_df
 from eda_module import aggregate_user_data, bivariate_analysis, compute_basic_metrics, create_and_normalize_columns,  segment_users_by_duration  
 
 # Load the data
-df = load_df()
+# URL of the CSV file on Google Drive
+csv_url = 'https://drive.google.com/file/d/1WKoMNNlQneudk5JY0JEvtRkCVYIUQYZK/view?usp=sharing'
+
+# Load the CSV file
+df = pd.read_csv(csv_url)
+# df = load_df()
 # df = df.head()
+
+#defining the numeric columns
+columns = ['Dur. (ms)', 'Avg RTT DL (ms)',
+       'Avg RTT UL (ms)', 'Avg Bearer TP DL (kbps)', 'Avg Bearer TP UL (kbps)',
+       'TCP DL Retrans. Vol (Bytes)', 'TCP UL Retrans. Vol (Bytes)',
+       'DL TP < 50 Kbps (%)', '50 Kbps < DL TP < 250 Kbps (%)',
+       '250 Kbps < DL TP < 1 Mbps (%)', 'DL TP > 1 Mbps (%)',
+       'UL TP < 10 Kbps (%)', '10 Kbps < UL TP < 50 Kbps (%)',
+       '50 Kbps < UL TP < 300 Kbps (%)', 'UL TP > 300 Kbps (%)',
+       'HTTP DL (Bytes)', 'HTTP UL (Bytes)', 'Activity Duration DL (ms)',
+       'Activity Duration UL (ms)', 'Dur. (ms).1', 'Nb of sec with 125000B < Vol DL',
+       'Nb of sec with 1250B < Vol UL < 6250B',
+       'Nb of sec with 31250B < Vol DL < 125000B',
+       'Nb of sec with 37500B < Vol UL',
+       'Nb of sec with 6250B < Vol DL < 31250B',
+       'Nb of sec with 6250B < Vol UL < 37500B',
+       'Nb of sec with Vol DL < 6250B', 'Nb of sec with Vol UL < 1250B',
+       'Social Media DL (Bytes)', 'Social Media UL (Bytes)',
+       'Google DL (Bytes)', 'Google UL (Bytes)', 'Email DL (Bytes)',
+       'Email UL (Bytes)', 'Youtube DL (Bytes)', 'Youtube UL (Bytes)',
+       'Netflix DL (Bytes)', 'Netflix UL (Bytes)', 'Gaming DL (Bytes)',
+       'Gaming UL (Bytes)', 'Other DL (Bytes)', 'Other UL (Bytes)',
+       'Total UL (Bytes)', 'Total DL (Bytes)']
+
+
+#preprocessing the data
+df = replace_missing_with_mean(df,columns)
+df = replace_outliers_with_mean(df, columns)
 
 # Numeric columns
 numeric_columns = [
--- scripts/new.py
+++ scripts/new.py
@@ -3,10 +3,13 @@ from load_data import load_df
 
 # Load the data
 df = load_df()
-df = df.head()
+# df = df.head()
+
+df.to_csv('tellco.csv', index=False)
 
 
 # # Aggregate the data
 # user_aggregates = aggregate_user_data(df)
 # print(user_aggregates)
-print(df.columns)
+# print(df.columns)
+print("ok")
